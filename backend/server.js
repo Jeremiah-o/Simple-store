2a63036d7947861d0cc2d5d576618e85c4f478e7
Jeremiah-o <omondijeremia2005@gmail.com>
2025-11-03 16:11:35 +0300

    mmmmmmmmmmm

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express()
 const port=process.env.PORT || 3000
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://simple-store-emad.onrender.com" ]
+  origin: ["http://localhost:5173", "https://simple-store-emad.onrender.com" ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
